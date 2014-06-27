4bdcf5c64532c38dc14cebc5f4031ea45a267272
wlstyy <taoyu@Taos-MacBook-Pro.local>
2014-06-26 19:57:10 -0700

    add wrong key tolerant

--- weather.js
+++ weather.js
@@ -21,24 +21,26 @@ http.createServer(function (req, res) {
   var count=0;
   for(var i=0;i<links.length;i++){
     //get weather information
-  	http.get(links[i], function(response) {
-  		response.on("data", function(chunk) {
+    http.get(links[i], function(response) {
+      response.on("data", function(chunk) {
         count++;
         //parse weather information
-    		var obj = JSON.parse(chunk);
-    		var city=obj['current_observation']['display_location']['full'];
-    		var temp=obj['current_observation']['temp_f']+"F";
-    		var weather=obj['current_observation']['weather'];
-    		var humidity=obj['current_observation']['relative_humidity'];
-    		var wind=obj['current_observation']['wind_string'];
-    		var pressure=obj['current_observation']['pressure_in']+"inHg";
-    		//var dew=obj['current_observation']['dewpoint_string'];
-    		var heat=obj['current_observation']['heat_index_string'];
-    		var winchill=obj['current_observation']['windchill_string'];
-    		var feel=obj['current_observation']['feelslike_f']+"F";
-    		var visi=obj['current_observation']['visibility_mi']+" mile";
-    		var preci_1hr=obj['current_observation']['precip_1hr_string'];
-    		var preci_today=obj['current_observation']['precip_today_string'];
+        var obj = JSON.parse(chunk);
+        if(typeof obj['current_observation'] !='undefined'){
+
+        var city=obj['current_observation']['display_location']['full'];
+        var temp=obj['current_observation']['temp_f']+"F";
+        var weather=obj['current_observation']['weather'];
+        var humidity=obj['current_observation']['relative_humidity'];
+        var wind=obj['current_observation']['wind_string'];
+        var pressure=obj['current_observation']['pressure_in']+"inHg";
+        //var dew=obj['current_observation']['dewpoint_string'];
+        var heat=obj['current_observation']['heat_index_string'];
+        var winchill=obj['current_observation']['windchill_string'];
+        var feel=obj['current_observation']['feelslike_f']+"F";
+        var visi=obj['current_observation']['visibility_mi']+" mile";
+        var preci_1hr=obj['current_observation']['precip_1hr_string'];
+        var preci_today=obj['current_observation']['precip_today_string'];
         //store in the list
         results.push({loc:city, wea:weather,tem:temp,hum:humidity
                       ,win:wind,press:pressure,hi:heat,wc:winchill
@@ -86,11 +88,16 @@ http.createServer(function (req, res) {
           res.write('</html>');
           res.end();
         }
-  		});
-	}).on('error', function(e) {
-  		console.log("Got error: " + e.message);
+      }else{
+        if(count==4){
+          console.log("Wrong Key");
+        }
+      }
+      });
+  }).on('error', function(e) {
+      console.log("Got error: " + e.message);
       //break;
-	});
+  });
   }
   //log information
   if(typeof log != 'undefined'&&String(log).length>0)
